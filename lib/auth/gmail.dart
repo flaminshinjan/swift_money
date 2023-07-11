b9a951bfc525356b82637adb01d7fbe3a1acee92
flaminshinjan <srikpatra.2008@gmail.com>
2023-07-11 16:49:28 +0530

    Working on onboarding screens

--- lib/auth/gmail.dart
+++ lib/auth/gmail.dart
@@ -1,16 +1,130 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
+import 'package:swift_money/auth/gmail.dart';
+import 'package:swift_money/auth/otp.dart';
 
-class GmailPage extends StatelessWidget {
+final TextEditingController _controller = TextEditingController();
+
+class GmailPage extends StatefulWidget {
   const GmailPage({super.key});
 
   @override
+  State<GmailPage> createState() => _GmailPageState();
+}
+
+class _GmailPageState extends State<GmailPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('Gmail'),
-        ],
-      ),
-    );
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 79,
+                ),
+                Image.asset(
+                  'assets/inv.png',
+                  height: 330,
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Enter your',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                    Text('Phone Number',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                    Text('We will share a verification code',
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w200,
+                            color: Colors.black))
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                TextFormField(
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                    labelText: 'Enter your mobile number',
+                    prefixText: '+91 ',
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'or connect with',
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => OtpPage()),
+                        );
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color.fromARGB(255, 237, 241, 245),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.asset(
+                            'assets/glogo.png',
+                            height: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => OtpPage()),
+                    );
+                  },
+                  color: Color.fromARGB(255, 33, 33, 40),
+                  textColor: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 160),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text(
+                    'Next',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/auth/otp.dart
+++ lib/auth/otp.dart
@@ -7,12 +7,41 @@ class OtpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('OTP'),
+        toolbarHeight: 80,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Image.asset(
+              'assets/back.png',
+            )),
       ),
-      body: Column(
-        children: [
-          const Text('OTP'),
-        ],
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Column(
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Enter the OTP number',
+                        style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                    Text('Verification code has been sent',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w200,
+                            color: Colors.black))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }

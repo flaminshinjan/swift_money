7b855273d21b9bcebf376b9750da96605a2f31d4
flaminshinjan <srikpatra.2008@gmail.com>
2023-07-20 11:07:47 +0530

    Changes Made

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Swift Money',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,

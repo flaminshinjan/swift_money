dbb449ad22d6b63248b454c4a4131c45d95ad2d7
flaminshinjan <srikpatra.2008@gmail.com>
2023-07-20 15:52:04 +0530

    Edit Goal Page added.

---
+++ lib/invfinal/editgoal.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditGoal extends StatefulWidget {
+  const EditGoal({super.key});
+
+  @override
+  State<EditGoal> createState() => _EditGoalState();
+}
+
+class _EditGoalState extends State<EditGoal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
